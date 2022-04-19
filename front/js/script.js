fe5d77357b4b6f9404e043dfaee3ec50e9d84b8e
Madi <Madison-CF@hotmail.fr>
2022-04-19 14:58:54 +0200

    initial commit

---
+++ front/js/script.js
@@ -0,0 +1,11 @@
+class Products {
+    constructor (colors, id, name, price, imageURL, description, altTxt){
+    this.colors = colors,
+    this.id = id,
+    this.name = name,
+    this.price = price,
+    this.imageURL = imageURL,
+    this.description = description,
+    this.altTxt = altTxt
+    }
+}
